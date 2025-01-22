d93b390680ed964f2432cb60df289c798075179e
ColinKessler <colinkessler00@gmail.com>
2025-01-22 19:55:54 +0000

    new alpha = 10 set

--- Alsomitra_NNCS/Robustness Training/main.py
+++ Alsomitra_NNCS/Robustness Training/main.py
@@ -111,9 +111,9 @@ def main(epsilon,basetraining):
 
 # Entry point of the script
 if __name__ == "__main__":
-    # main(0.005,True)
-    # main(0.01,True)
-    # main(0.02,True)
+    main(0.005,True)
+    main(0.01,True)
+    main(0.02,True)
     main(0.04,True)
-    # main(0.08,True)
+    main(0.08,True)
 # 
