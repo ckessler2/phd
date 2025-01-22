58495bfba632bac7c9535e6131fdfb4101e47c8b
ColinKessler <colinkessler00@gmail.com>
2025-01-22 13:50:42 +0000

    fixed data

--- Alsomitra_NNCS/Alsomitra_Control_Simulation.m
+++ Alsomitra_NNCS/Alsomitra_Control_Simulation.m
@@ -24,7 +24,7 @@ function Alsomitra_Control_simulation(network1)
     % Ss = data2(2,:);
     
     % NN or PID controller
-    nnc =  true;
+    nnc =  false;
     
     ObjectiveFunction = @Alsomitra_nondim;
     
