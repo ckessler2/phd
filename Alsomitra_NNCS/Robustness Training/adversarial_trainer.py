fixed vehicle spec, main training script, added lipschitz calculation to verify counterexamples. And swallow stuff

Alsomitra_NNCS/Robustness Training/adversarial_trainer.py
@@ -82,7 +82,7 @@ class AdversarialTrainer:
         #     adversarial_x, 0, 1
         # )  # Ensure values stay within [0, 1] bounds
 
-    def train_with_adversarial_examples(self, train_dataset, epochs=10, callbacks=None, alpha=10):
+    def train_with_adversarial_examples(self, train_dataset, epochs=1, callbacks=None, alpha=5):
         """
         Trains the model on both normal and adversarial examples to enforce robustness within an epsilon-ball.
 
@@ -128,15 +128,18 @@ class AdversarialTrainer:
                 # history2["history"]["val_loss"].append(val_loss)
                 history2.add_entry(loss, val_loss)
                 
-                with tf.GradientTape() as tape:
+                with tf.GradientTape(persistent=True) as tape:
                     adversarial_y_batch = self.model(adversarial_x_batch, training=True)
                 
                     loss = alpha*LipschitzLoss(x_batch, adversarial_x_batch, y_batch, adversarial_y_batch)
                     
                     grads = tape.gradient(loss, self.model.trainable_weights)
-    
-                    tf.keras.optimizers.Adam().apply_gradients(zip(grads, self.model.trainable_weights))
-
+                    a = str(grads[1])
+                    if "nan" not in a: 
+                        tf.keras.optimizers.Adam().apply_gradients(zip(grads, self.model.trainable_weights))
+                        
+                    del tape
+                    
             for callback in callbacks:
                 callback.on_epoch_end(epoch)
 

Swallow_Sensor/Plot_ROC.py
@@ -11,7 +11,7 @@ from sklearn.metrics import roc_curve, auc
 
 import tensorflow as tf
 
-# Load your ONNX model
+# Load model - should be onnx but I couldn't get it to work
 model = tf.keras.models.load_model('Swallow_NN_Classifier.keras')
 
 testX = np.load("testX.npy")
@@ -32,13 +32,19 @@ fpr, tpr, thresholds = roc_curve(testy, positive_class_probabilities)
 roc_auc = auc(fpr, tpr)
 
 # Plot ROC curve
-plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+tfont = {'fontname':'Times New Roman'} 
+
+
+plt.figure
+plt.plot([0, 1], [0, 1], color='black', lw=2, label='Random classifier')
+plt.plot(fpr, tpr, color='orange', lw=2, label='NN classifier \n(area = %0.2f)' % roc_auc)
+plt.plot([0,1], [1,1], color='black', lw=2, linestyle=':', label='Perfect classifier')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('ROC (Validation Data)')
+plt.title('ROC',**tfont)
 plt.legend(loc="lower right")
+ax = plt.gca()
+ax.set_aspect('equal', adjustable='box')
 plt.show()