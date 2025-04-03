d8c89d90e90b72f28f7f3ba6f2a256967e85ccd7
ColinKessler <colinkessler00@gmail.com>
2025-04-03 13:53:40 +0100

    stuff

--- blipblop/PDE_version.m
+++ blipblop/PDE_version.m
@@ -23,12 +23,19 @@ x0 = [0.0150000000000000; 1; 0; 0.0150000000000000; 0; 0; 0; 0; 0; 0; 0; 0; 0];
 
 % get result
 x1 = x(:,1);
+x2 = x(:,2);
 x4 = x(:,4);
 PVacI = x(:,7);
 fExt1 = x(:,8); fExt2 = x(:,9);
 
+states = get_states(t, x1, x4, 0.015, 0.015, 0.2);
+
 % Plots
-tiledlayout("flow"); nexttile;
+figure
+fig = tiledlayout("flow"); nexttile;
+
+plot(t,x2*1000,'linewidth',2)
+title("X2 vs Time"); nexttile;
 
 plot(t,x1*1000,'linewidth',2); hold on
 plot(t,x4*1000,'linewidth',2)
@@ -36,7 +43,8 @@ xlabel('Time [s]'); ylabel('Displacement [mm]')
 legend('first','second'); title("Displacement vs Time")
 
 nexttile;
-title('Status vs Time, to be implemented')
+plot(t, states)
+ylim([1 4]); title("State vs Time")
 
 nexttile;
 plot(t,PVacI*1e-3)
@@ -79,20 +87,20 @@ function dxdt = system_eqns(t, x)
     % Check conditions and update force/pressure appropriately
     if x4 > length(2) * stage && x1 > length(1) * stage % Both extended
         dPVacI = PVAC + (8*MDOTVAC^2)/(RHO*(pi*holeDiam(1)^2)^2) - 0.5*RHO*((diam(2)/holeDiam(2))^2*x5)^2;
-        dfExt1 = -(pi*holeDiam(1)^2)/4*PVAC - (pi*holeDiam(2)^2)/4*PVacI;
-        dfExt2 = -(pi*holeDiam(2)^2)/4*PVacI + (pi*holeDiam(3)^2)/4*PATM;
+        dfExt1 = -(pi*holeDiam(1)^2)/4*PVAC - (pi*holeDiam(2)^2)/4*dPVacI;
+        dfExt2 = -(pi*holeDiam(2)^2)/4*dPVacI + (pi*holeDiam(3)^2)/4*PATM;
     elseif x4 < length(2) * stage && x1 > length(1) * stage % 2nd collapsed and 1st still extending
         dPVacI = PVAC + (8*MDOTVAC^2)/(RHO*(pi*holeDiam(1)^2)^2) - 0.5*RHO*((diam(2)/holeDiam(2))^2*x5)^2;
-        dfExt1 = -(pi*holeDiam(1)^2)/4*PVAC - (pi*holeDiam(2)^2)/4*PVacI;
-        dfExt2 = -(pi*holeDiam(2)^2)/4*PVacI;
+        dfExt1 = -(pi*holeDiam(1)^2)/4*PVAC - (pi*holeDiam(2)^2)/4*dPVacI;
+        dfExt2 = -(pi*holeDiam(2)^2)/4*dPVacI;
     elseif x4 < length(2) * stage && x1 < length(1) * stage % Both collapsed
         dPVacI = 0;
-        dfExt1 = -(pi*holeDiam(1)^2)/4*PVacI;
-        dfExt2 = -(pi*holeDiam(2)^2)/4*PVacI + 1.2000; % Adjust this constant based on actual parameters
+        dfExt1 = -(pi*holeDiam(1)^2)/4*dPVacI;
+        dfExt2 = -(pi*holeDiam(2)^2)/4*dPVacI + 1.2000; % Adjust this constant based on actual parameters
     elseif x4 > length(2) * stage && x1 < length(1) * stage % 2nd fully extended and 1st still collapsed
         dPVacI = PATM + 0.5*RHO*((diam(2)/holeDiam(2))^2*x5)^2;
-        dfExt1 = (pi*holeDiam(1)^2)/4*PVacI;
-        dfExt2 = -(pi*holeDiam(2)^2)/4*PVacI + (pi*holeDiam(3)^2)/4*PATM;
+        dfExt1 = (pi*holeDiam(1)^2)/4*dPVacI;
+        dfExt2 = -(pi*holeDiam(2)^2)/4*dPVacI + (pi*holeDiam(3)^2)/4*PATM;
     end
 
     % Updating equation for alpha1
@@ -116,4 +124,22 @@ function dxdt = system_eqns(t, x)
     dxdt(11) = dalpha2;
     dxdt(12) = dalpha3;
     dxdt(13) = dalpha4;
+end
+
+function states = get_states(t, x1, x4, length1, length2, stage)
+    for iterCount = 2:length(t)+1 % initial conditions are iterCount = 1
+
+        if x4(iterCount-1) > length2*stage && x1(iterCount-1) > length1*stage % both extended: vac & atm
+            states(iterCount-1) = 1;
+    
+        elseif x4(iterCount-1) < length2*stage && x1(iterCount-1) > length1*stage % 2nd collapsed and 1st still collapsing: vac 
+            states(iterCount-1) = 2;
+    
+        elseif x4(iterCount-1) < length2*stage && x1(iterCount-1) < length1*stage % both collapsed: intermediate pressure
+            states(iterCount-1) = 3;
+    
+        elseif x4(iterCount-1) > length2*stage && x1(iterCount-1) < length1*stage % 2nd fully extended and 1st still collapsed
+            states(iterCount-1) = 4;
+        end
+    end
 end
