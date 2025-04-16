5f1d1579fdf2dab4436f18d1a9df870e357a86ed
ColinKessler <colinkessler00@gmail.com>
2025-04-16 05:32:38 +0100

    stuff

--- Alsomitra_NNCS/no_error/Reachability/run_reachability.m
+++ Alsomitra_NNCS/no_error/Reachability/run_reachability.m
@@ -1,5 +1,5 @@
-[completed1, R1,simRes1, dims1] = Alsomitra_Closed_Loop_Reachability("base_model_denorm.onnx",0,16);
-% save 'base_0.mat' R1
-% 
+[completed1, R1,simRes1, dims1] = Alsomitra_Closed_Loop_Reachability("base_model_denorm.onnx",0,32);
+save 'base_0.mat' R1
+
 % [completed2, R2,simRes2, dims2] = Alsomitra_Closed_Loop_Reachability("adversarial_model_005_denorm.onnx",0,8);
 % save '005_0.mat' R2
