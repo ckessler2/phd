09ba593fbde115e74cd848e484b80dc3f2f35b59
ColinKessler <colinkessler00@gmail.com>
2025-05-07 17:28:08 +0100

    Update plot_confusion.m

--- Swallow_Sensor/plot_confusion.m
+++ Swallow_Sensor/plot_confusion.m
@@ -28,6 +28,14 @@ cm.XLabel = 'Predicted Class';
 cm.YLabel = 'True Class';
 cm.FontName = 'Times New Roman';  % Set up the font face
 
+% cm.DiagonalColor = [49, 84, 122]/255;
+% cm.OffDiagonalColor = ([110, 28, 99]+130)/255;
+
+color = [92, 1, 166]/255;
+
+cm.DiagonalColor = color;
+cm.OffDiagonalColor = color * 1;
+
 data2 =  csvread("NN2_1_loss.csv");
 acc1 = data2(1,:);
 acc2 = data2(2,:);
@@ -41,57 +49,57 @@ acc4 = data3(2,:);
 loss3 = data3(3,:);
 loss4 = data3(4,:);
 
-figure
-t = tiledlayout(2,2); nexttile
-
-plot(1:50,acc1,"LineWidth",1); hold on
-plot(1:50,acc2,"LineWidth",1); xlim([0 50]) 
-
-% xlabel("Training Epoch")
-ylabel("Accuracy")
-box on; pbaspect([3 1 1]);ylim([0 1]); xlim([0 50]) 
-yticks([0 0.5 1])
-yticklabels([0 0.5 1])
-xticks([0 10 20 30 40 50])
-xticklabels([])
-
-legend("Train", "Validation")
-
-grid on; nexttile
-
-plot(1:50,acc3,"LineWidth",1); hold on
-plot(1:50,acc4,"LineWidth",1); 
-
-% xlabel("Training Epoch")
+% figure
+% t = tiledlayout(2,2); nexttile
+% 
+% plot(1:50,acc1,"LineWidth",1); hold on
+% plot(1:50,acc2,"LineWidth",1); xlim([0 50]) 
+% 
+% % xlabel("Training Epoch")
 % ylabel("Accuracy")
-box on; pbaspect([3 1 1]); ylim([0 1]); xlim([0 50]) 
-yticks([0 0.5 1])
-yticklabels([])
-xticks([0 10 20 30 40 50])
-xticklabels([])
-
-grid on; nexttile
-
-plot(1:50,loss1,"LineWidth",1); hold on
-plot(1:50,loss2,"LineWidth",1); xlim([0 50]); ylim([0.5 2])
-
-xlabel("Training Epoch")
-ylabel("Loss")
-box on; pbaspect([3 1 1])
-yscale("log"); xlim([0 50]) 
-xticks([0 10 20 30 40 50])
-yticks([0.5 1 2])
-
-grid on; nexttile
-
-plot(1:50,loss3,"LineWidth",1); hold on
-plot(1:50,loss4,"LineWidth",1); xlim([0 50]) ; ylim([0.5 2])
-
-xlabel("Training Epoch")
+% box on; pbaspect([3 1 1]);ylim([0 1]); xlim([0 50]) 
+% yticks([0 0.5 1])
+% yticklabels([0 0.5 1])
+% xticks([0 10 20 30 40 50])
+% xticklabels([])
+% 
+% legend("Train", "Validation")
+% 
+% grid on; nexttile
+% 
+% plot(1:50,acc3,"LineWidth",1); hold on
+% plot(1:50,acc4,"LineWidth",1); 
+% 
+% % xlabel("Training Epoch")
+% % ylabel("Accuracy")
+% box on; pbaspect([3 1 1]); ylim([0 1]); xlim([0 50]) 
+% yticks([0 0.5 1])
+% yticklabels([])
+% xticks([0 10 20 30 40 50])
+% xticklabels([])
+% 
+% grid on; nexttile
+% 
+% plot(1:50,loss1,"LineWidth",1); hold on
+% plot(1:50,loss2,"LineWidth",1); xlim([0 50]); ylim([0.5 2])
+% 
+% xlabel("Training Epoch")
 % ylabel("Loss")
-box on; pbaspect([3 1 1])
-yscale("log"); xlim([0 50]) 
-xticks([0 10 20 30 40 50])
-yticks([0.5 1 2])
-yticklabels([])
-grid on
+% box on; pbaspect([3 1 1])
+% yscale("log"); xlim([0 50]) 
+% xticks([0 10 20 30 40 50])
+% yticks([0.5 1 2])
+% 
+% grid on; nexttile
+% 
+% plot(1:50,loss3,"LineWidth",1); hold on
+% plot(1:50,loss4,"LineWidth",1); xlim([0 50]) ; ylim([0.5 2])
+% 
+% xlabel("Training Epoch")
+% % ylabel("Loss")
+% box on; pbaspect([3 1 1])
+% yscale("log"); xlim([0 50]) 
+% xticks([0 10 20 30 40 50])
+% yticks([0.5 1 2])
+% yticklabels([])
+% grid on
